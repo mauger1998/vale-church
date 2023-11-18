018ee9af29ffb35ac76c52582a4c63e1395188c8
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-11-18 14:44:04 +0000

    Make form more accesible and add bookings email

--- facilities/index.js
+++ facilities/index.js
@@ -1,40 +1,52 @@
 gsap.registerPlugin(ScrollTrigger)
 
-const menuIcon = document.querySelector('header img:not(.cross)')
+const menuIcon = document.querySelector('header img:not(.cross, .logo)')
 const crossIcon = document.querySelector('.cross')
 const dropdown = document.querySelector('nav ul')
+const dropdownLinks = document.querySelectorAll('nav ul a')
+
+const html = document.querySelector('html')
 
 menuIcon.addEventListener('click', () => {
-    dropdown.classList.add('active')
+	dropdown.classList.add('active')
+	html.style.overflowY = 'hidden'
 })
 
 crossIcon.addEventListener('click', () => {
-    dropdown.classList.remove('active')
+	dropdown.classList.remove('active')
+	html.style.overflowY = 'auto'
+})
+
+dropdownLinks.forEach((link) => {
+	link.addEventListener('click', () => {
+		dropdown.classList.remove('active')
+		html.style.overflowY = 'auto'
+	})
 })
 // swiper element
 const swiperEl = document.querySelector('.swiper-one')
 
 // swiper parameters
 const swiperParams = {
-    slidesPerView: 2.2,
-    navigation: true,
-    breakpoints: {
-        375: {
-            slidesPerView: 3.2,
-        },
-        640: {
-            slidesPerView: 5.2,
-            spaceBetween: 16,
-        },
-        1024: {
-            slidesPerView: 6.2,
-        },
-    },
-    on: {
-        init() {
-            // ...
-        },
-    },
+	slidesPerView: 2.2,
+	navigation: true,
+	breakpoints: {
+		375: {
+			slidesPerView: 3.2
+		},
+		640: {
+			slidesPerView: 5.2,
+			spaceBetween: 16
+		},
+		1024: {
+			slidesPerView: 6.2
+		}
+	},
+	on: {
+		init() {
+			// ...
+		}
+	}
 }
 
 // now we need to assign all parameters to Swiper element
@@ -48,24 +60,24 @@ const swiperEl2 = document.querySelector('.swiper-two')
 
 // swiper parameters
 const swiperParams2 = {
-    slidesPerView: 2.2,
-    spaceBetween: 0,
-    navigation: true,
+	slidesPerView: 2.2,
+	spaceBetween: 0,
+	navigation: true,
 
-    breakpoints: {
-        640: {
-            slidesPerView: 5.2,
-            spaceBetween: 16,
-        },
-        1024: {
-            slidesPerView: 6.2,
-        },
-    },
-    on: {
-        init() {
-            // ...
-        },
-    },
+	breakpoints: {
+		640: {
+			slidesPerView: 5.2,
+			spaceBetween: 16
+		},
+		1024: {
+			slidesPerView: 6.2
+		}
+	},
+	on: {
+		init() {
+			// ...
+		}
+	}
 }
 
 // now we need to assign all parameters to Swiper element
@@ -76,91 +88,91 @@ swiperEl2.initialize()
 
 // GSAP
 const getBrowserName = () => {
-    let browserInfo = navigator.userAgent
-    let browser
-    if (browserInfo.includes('Opera') || browserInfo.includes('Opr')) {
-        browser = 'Opera'
-    } else if (browserInfo.includes('Edg')) {
-        browser = 'Edge'
-    } else if (browserInfo.includes('Chrome')) {
-        browser = 'Chrome'
-    } else if (browserInfo.includes('Safari')) {
-        browser = 'Safari'
-    } else if (browserInfo.includes('Firefox')) {
-        browser = 'Firefox'
-    } else {
-        browser = 'unknown'
-    }
-    return browser
+	let browserInfo = navigator.userAgent
+	let browser
+	if (browserInfo.includes('Opera') || browserInfo.includes('Opr')) {
+		browser = 'Opera'
+	} else if (browserInfo.includes('Edg')) {
+		browser = 'Edge'
+	} else if (browserInfo.includes('Chrome')) {
+		browser = 'Chrome'
+	} else if (browserInfo.includes('Safari')) {
+		browser = 'Safari'
+	} else if (browserInfo.includes('Firefox')) {
+		browser = 'Firefox'
+	} else {
+		browser = 'unknown'
+	}
+	return browser
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    if (getBrowserName() !== 'Safari') {
-        const allFancyText = document.querySelectorAll('span:not(h1 span)')
+	if (getBrowserName() !== 'Safari') {
+		const allFancyText = document.querySelectorAll('span:not(h1 span)')
 
-        allFancyText.forEach((text) => {
-            gsap.set(text, {
-                clipPath: 'polygon(0 0, 100% 0, 0 0, 0% 100%)',
-            })
-            gsap.to(text, {
-                scrollTrigger: {
-                    trigger: text,
-                    start: 'top 90%',
-                    toggleActions: 'play play play reverse',
-                },
-                clipPath: 'polygon(0 0, 115% 0, 100% 100%, -15% 100%)',
-                ease: 'power1.inOut',
-            })
-        })
-        gsap.set('h1 span', {
-            clipPath: 'polygon(0 0, 100% 0, 0 0, 0% 100%)',
-        })
-        gsap.set('h1', {
-            clipPath: 'polygon(-20% 100%, 100% 100%, 100% 100%, 0 100%)',
-        })
+		allFancyText.forEach((text) => {
+			gsap.set(text, {
+				clipPath: 'polygon(0 0, 100% 0, 0 0, 0% 100%)'
+			})
+			gsap.to(text, {
+				scrollTrigger: {
+					trigger: text,
+					start: 'top 90%',
+					toggleActions: 'play play play reverse'
+				},
+				clipPath: 'polygon(0 0, 115% 0, 100% 100%, -15% 100%)',
+				ease: 'power1.inOut'
+			})
+		})
+		gsap.set('h1 span', {
+			clipPath: 'polygon(0 0, 100% 0, 0 0, 0% 100%)'
+		})
+		gsap.set('h1', {
+			clipPath: 'polygon(-20% 100%, 100% 100%, 100% 100%, 0 100%)'
+		})
 
-        gsap.to('h1', {
-            clipPath: 'polygon(0 0, 100% 0, 100% 200%, -10% 100%)',
-            ease: 'power1.inOut',
-            onComplete: () => {
-                gsap.to('h1 span', {
-                    delay: 0.4,
-                    clipPath: 'polygon(0 0, 100% 0, 100% 150%, -100% 100%)',
-                    ease: 'power1.inOut',
-                })
-            },
-        })
-    }
+		gsap.to('h1', {
+			clipPath: 'polygon(0 0, 100% 0, 100% 200%, -10% 100%)',
+			ease: 'power1.inOut',
+			onComplete: () => {
+				gsap.to('h1 span', {
+					delay: 0.4,
+					clipPath: 'polygon(0 0, 100% 0, 100% 150%, -100% 100%)',
+					ease: 'power1.inOut'
+				})
+			}
+		})
+	}
 })
 
 // Whats on
 
 // Quote Fetch
 async function fetchWhatsOn() {
-    const response = await fetch(
-        `https://pxvhzoh0.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22whatsOnSection%22%5D+%7B%0A++title%2C%0A++whatsOnContent%5B%5D+%7B%0A++++text%2C%0A++++title%2C%0A++++%22imgUrl%22%3A+image.asset-%3Eurl%0A++%7D%0A%7D`
-    )
-    const event = await response.json()
-    return event
+	const response = await fetch(
+		`https://pxvhzoh0.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22whatsOnSection%22%5D+%7B%0A++title%2C%0A++whatsOnContent%5B%5D+%7B%0A++++text%2C%0A++++title%2C%0A++++%22imgUrl%22%3A+image.asset-%3Eurl%0A++%7D%0A%7D`
+	)
+	const event = await response.json()
+	return event
 }
 
 fetchWhatsOn().then((event) => {
-    const { result } = event
+	const { result } = event
 
-    const resolvedResult = result[0].whatsOnContent
+	const resolvedResult = result[0].whatsOnContent
 
-    const whatsOnGridContainer = document.querySelector('.whats-on-grid')
+	const whatsOnGridContainer = document.querySelector('.whats-on-grid')
 
-    resolvedResult.forEach((result) => {
-        const card = document.createElement('div')
-        card.classList.add('card')
+	resolvedResult.forEach((result) => {
+		const card = document.createElement('div')
+		card.classList.add('card')
 
-        card.innerHTML = /*html*/ `
+		card.innerHTML = /*html*/ `
 
                 <img src="${result.imgUrl}" alt="Vale Church Event" />
                 <h3>${result.title}</h3>
                 <p class="no-cap">${result.text}</p>
         `
-        whatsOnGridContainer.append(card)
-    })
+		whatsOnGridContainer.append(card)
+	})
 })
--- history-and-media/index.js
+++ history-and-media/index.js
@@ -1,22 +1,22 @@
 gsap.registerPlugin(ScrollTrigger)
 
 const getBrowserName = () => {
-    let browserInfo = navigator.userAgent
-    let browser
-    if (browserInfo.includes('Opera') || browserInfo.includes('Opr')) {
-        browser = 'Opera'
-    } else if (browserInfo.includes('Edg')) {
-        browser = 'Edge'
-    } else if (browserInfo.includes('Chrome')) {
-        browser = 'Chrome'
-    } else if (browserInfo.includes('Safari')) {
-        browser = 'Safari'
-    } else if (browserInfo.includes('Firefox')) {
-        browser = 'Firefox'
-    } else {
-        browser = 'unknown'
-    }
-    return browser
+	let browserInfo = navigator.userAgent
+	let browser
+	if (browserInfo.includes('Opera') || browserInfo.includes('Opr')) {
+		browser = 'Opera'
+	} else if (browserInfo.includes('Edg')) {
+		browser = 'Edge'
+	} else if (browserInfo.includes('Chrome')) {
+		browser = 'Chrome'
+	} else if (browserInfo.includes('Safari')) {
+		browser = 'Safari'
+	} else if (browserInfo.includes('Firefox')) {
+		browser = 'Firefox'
+	} else {
+		browser = 'unknown'
+	}
+	return browser
 }
 // Modal
 
@@ -26,61 +26,72 @@ const closeButton = document.querySelector('#close-modal')
 
 // "Show the dialog" button opens the dialog modally
 showButton.addEventListener('click', () => {
-    dialog.showModal()
+	dialog.showModal()
 })
 
 // "Close" button closes the dialog
 closeButton.addEventListener('click', () => {
-    dialog.close()
+	dialog.close()
 })
 
-const menuIcon = document.querySelector('header img:not(.cross)')
+const menuIcon = document.querySelector('header img:not(.cross, .logo)')
 const crossIcon = document.querySelector('.cross')
+const dropdownLinks = document.querySelectorAll('nav ul a')
+
 const dropdown = document.querySelector('nav ul')
+const html = document.querySelector('html')
 
 menuIcon.addEventListener('click', () => {
-    dropdown.classList.add('active')
+	dropdown.classList.add('active')
+	html.style.overflowY = 'hidden'
 })
 
 crossIcon.addEventListener('click', () => {
-    dropdown.classList.remove('active')
+	dropdown.classList.remove('active')
+	html.style.overflowY = 'auto'
+})
+dropdownLinks.forEach((link) => {
+	link.addEventListener('click', () => {
+		dropdown.classList.remove('active')
+		html.style.overflowY = 'auto'
+	})
 })
 
 window.addEventListener('DOMContentLoaded', () => {
-    if (getBrowserName() !== 'Safari') {
-        const allFancyText = document.querySelectorAll('span:not(h1 span)')
+	if (getBrowserName() !== 'Safari') {
+		const allFancyText = document.querySelectorAll('span:not(h1 span)')
 
-        allFancyText.forEach((text) => {
-            gsap.set(text, {
-                clipPath: 'polygon(0 0, 100% 0, 0 0, 0% 100%)',
-            })
-            gsap.to(text, {
-                scrollTrigger: {
-                    trigger: text,
-                    start: 'top 90%',
-                    toggleActions: 'play play play reverse',
-                },
-                clipPath: 'polygon(0 0, 115% 0, 100% 100%, -15% 100%)',
-                ease: 'power1.inOut',
-            })
-        })
-        gsap.set('h1 span', {
-            clipPath: 'polygon(0 0, 100% 0, 0 0, 0% 100%)',
-        })
-        gsap.set('h1', {
-            clipPath: 'polygon(2% 100%, 100% 100%, 100% 100%, 0 100%)',
-        })
+		allFancyText.forEach((text) => {
+			gsap.set(text, {
+				clipPath: 'polygon(0 0, 100% 0, 0 0, 0% 100%)'
+			})
+			gsap.to(text, {
+				scrollTrigger: {
+					trigger: text,
+					start: 'top 90%',
+					toggleActions: 'play play play reverse'
+				},
+				clipPath: 'polygon(0 0, 115% 0, 100% 100%, -15% 100%)',
+				ease: 'power1.inOut'
+			})
+		})
+		gsap.set('h1 span', {
+			clipPath: 'polygon(0 0, 100% 0, 0 0, 0% 100%)'
+		})
+		gsap.set('h1', {
+			clipPath: 'polygon(2% 100%, 100% 100%, 100% 100%, 0 100%)'
+		})
 
-        gsap.to('h1', {
-            clipPath: 'polygon(0 0, 100% 0, 100% 100%, -10% 100%)',
-            ease: 'power1.inOut',
-            onComplete: () => {
-                gsap.to('h1 span', {
-                    delay: 0.4,
-                    clipPath: 'polygon(0 0, 100% 0, 100% 100%, -10% 100%)',
-                    ease: 'power1.inOut',
-                })
-            },
-        })
-    }
+		gsap.to('h1', {
+			clipPath: 'polygon(0 0, 100% 0, 100% 100%, -10% 100%)',
+			ease: 'power1.inOut',
+			onComplete: () => {
+				gsap.to('h1 span', {
+					delay: 0.4,
+					clipPath: 'polygon(0 0, 100% 0, 100% 100%, -10% 100%)',
+					ease: 'power1.inOut'
+				})
+			}
+		})
+	}
 })
